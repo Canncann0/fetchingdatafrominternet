129f81ac824dc913175c36ec5fdec1162ed790fc
Dogancan <turgutdogancan0@gmail.com>
2025-11-19 19:31:18 +0300

    Initial Commit

---
+++ fetchingdatafrominternet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  fetchingdatafrominternet
+//
+//  Created by Dogancan turgut on 19.11.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ fetchingdatafrominternet/fetchingdatafrominternetApp.swift
@@ -0,0 +1,17 @@
+//
+//  fetchingdatafrominternetApp.swift
+//  fetchingdatafrominternet
+//
+//  Created by Dogancan turgut on 19.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct fetchingdatafrominternetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
